d5f707ebcf84ff07a0718e7ee6e528fd14947987
ligaopeng123 <gaopeng@123>
2022-10-31 20:28:26 +0800

    RcMultiScreenPlayer RcWebRTCPlayer支持

--- packages/rc-multi-player/src/index.ts
+++ packages/rc-multi-player/src/index.ts
@@ -1,5 +1,8 @@
 export { default as RcMultiPlayer } from './Player/MultiPlayer';
 export { MultiPlayerComProps, MediaDataSource, Config, MultiPlayerRobustness } from "@gaopeng123/multi-player";
+// 混个播放器
 export { default as RcMultiScreenPlayer } from './MultiScreenPlayer';
+// WebRTC播放器
 export { default as RcWebRTCPlayer } from './Player/WebRTCPlayer';
+// flv播放器 后续支持h265解码
 export { default as RcFlyPlayer } from './Player/FlvPlayer';
