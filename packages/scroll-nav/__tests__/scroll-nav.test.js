3c0dbe028e7f171dce9b32ece351b080b5353f2b
ligaopeng123 <1170234932@qq.com>
2023-03-08 20:03:39 +0800

    test

--- packages/scroll-nav/__tests__/scroll-nav.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const scrollNav = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(scrollNav(), 'Hello from scrollNav');
-console.info("scrollNav tests passed");
