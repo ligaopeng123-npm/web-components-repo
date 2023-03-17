0040388591e3cfe8239efccf63e9b7ad5b0dba23
ligaopeng123 <1170234932@qq.com>
2023-03-17 15:54:09 +0800

    feat: scroll-nav

---
+++ packages/rc-paginated-list/src/index.ts
@@ -0,0 +1,15 @@
+/** ********************************************************************
+ *
+ * @模块名称: PaginatedList
+ *
+ * @模块用途: PaginatedList  分页列表
+ *
+ * @date: 2022/10/9 8:31
+ *
+ * @版权所有: pgli
+ *
+ ********************************************************************* */
+export { default as RcPaginatedList } from './PaginatedList';
+export { default as RcDownLoading } from './DownLoading';
+export { default as RcTopLoading } from './TopLoading';
+export { default as EmptyList } from './Empty';
---
+++ packages/scroll-nav/__tests__/scroll-nav.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const scrollNav = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(scrollNav(), 'Hello from scrollNav');
+console.info("scrollNav tests passed");
