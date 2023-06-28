a3b1ca39183b834e9c60360b30e064bfb6f372a9
ligaopeng123 <1170234932@qq.com>
2023-06-28 15:43:04 +0800

    fix: 开发环境配置

--- packages/scroll-nav/webpack.config.js
+++ packages/scroll-nav/webpack.config.js
@@ -2,7 +2,7 @@ const { merge } = require("webpack-merge");
 const base = require("../../webpack.base.config");
 
 module.exports = merge(base(__dirname), {
-    entry: './__tests__/index.tsx',
+    entry: './__tests__/index.ts',
     devServer: {
         port: 5513,
         proxy: {
