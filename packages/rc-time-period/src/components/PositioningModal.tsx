7cd4f79779593553f8a233d5ee93d88bc0eeb1ab
ligaopeng123 <1170234932@qq.com>
2023-07-24 18:09:32 +0800

    fix: import * as ReactTooltip from 'react-tooltip';

--- packages/rc-time-period/src/components/PositioningModal.tsx
+++ packages/rc-time-period/src/components/PositioningModal.tsx
@@ -14,7 +14,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { getWeekTimeChangePosition } from "../utils";
 import { PositionModalInterface, PositionModalProps } from "../interface";
-import { Tooltip as ReactTooltip } from "react-tooltip";
+import * as ReactTooltip from 'react-tooltip';
 import '../index.less';
 
 /**
@@ -56,7 +56,7 @@ export const PositioningModal: React.FC<PositionModalProps> = (props) => {
 
     return (
         <Fragment>
-            <ReactTooltip
+            <ReactTooltip.Tooltip
                 isOpen={visible}
                 clickable={true}
                 anchorSelect={`#${modalKey || 'position-modal'}`}
@@ -91,7 +91,7 @@ export const PositioningModal: React.FC<PositionModalProps> = (props) => {
                             onConfirm(e);
                         }}>{okText || `保存`}</button>
                 </div>
-            </ReactTooltip>
+            </ReactTooltip.Tooltip>
             <div
                 id={modalKey || "position-modal"}
                 data-tooltip-id={modalKey || "position-modal"}
