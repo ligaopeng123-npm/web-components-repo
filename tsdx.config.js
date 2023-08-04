3015b98ca1aa7fa9bd847bcf5fbcb77f7bcd9a45
ligaopeng123 <1170234932@qq.com>
2023-08-04 15:05:48 +0800

    feat: extract字段可配置，允许将css打入到js中

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,3 +1,4 @@
+import {isUndefined} from "@gaopeng123/utils";
 const images = require('@rollup/plugin-image');
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
@@ -5,6 +6,16 @@ const cssnano = require('cssnano');
 // const url = require('rollup-plugin-url');
 // const svgr = require('@svgr/rollup');
 const cssUrl = require('postcss-url');
+
+/**
+ * 子模块使用
+ *
+ *  module.exports = {
+ *      rollup: (config, options)=> tsdxConfig(config, Object.assign({}, options, {writeMeta: true}))
+ *  }
+ *
+ */
+
 
 module.exports = function (config, options) {
     config.plugins = [
@@ -26,9 +37,11 @@ module.exports = function (config, options) {
             inject: true, // 是否主动注入css到js中(设置为true,则在引用组件的时候,就无需再单独引入css了)
             modules: false, // 使用css modules
             camelCase: true, // 支持驼峰
-            extract: !!options.writeMeta, //
+            // 如果为true则会把样式文件单独打出，默认为true; 如果为绝对路径，则打出的文件命名为当前命名
+            extract: isUndefined(options.extract) ? !!options.writeMeta : options.extract,
+            // extract: '/index.css',
             autoModules: true,
-            css: true,
+            css: true, // 是否使用css
             less: true, // 是否使用less,
             sass: true, // 是否使用sass
         })
