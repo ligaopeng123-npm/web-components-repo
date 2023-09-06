9dc44fcc421c20563887c1d93750f9c4631f123e
ligaopeng123 <1170234932@qq.com>
2023-09-06 09:57:21 +0800

    docs: 文档开发

--- stories/Header.stories.js
+++ stories/Header.stories.js
@@ -1,7 +1,7 @@
 import { Header } from './Header';
 
 export default {
-  title: 'Example/Header',
+  title: 'Example/总览',
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
