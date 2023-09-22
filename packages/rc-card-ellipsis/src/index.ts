60ebb0632ba90215bcf4a50f1237f2c3a9cc9c49
ligaopeng123 <1170234932@qq.com>
2023-09-22 10:52:21 +0800

    fix: 添加 导出 CardEllipsisMode

--- packages/rc-card-ellipsis/src/index.ts
+++ packages/rc-card-ellipsis/src/index.ts
@@ -1,2 +1,3 @@
 export { default as RcCardEllipsis } from './RcCardEllipsis';
 export type { RcCardEllipsisProps } from './RcCardEllipsis';
+export type { CardEllipsisMode } from "@gaopeng123/card-ellipsis";
