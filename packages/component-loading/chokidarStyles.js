73e1f8b7e0719110977f963170ce59f0ef5ab5ac
ligaopeng123 <1170234932@qq.com>
2024-02-20 15:50:56 +0800

    feat: styles 错误监听

--- packages/component-loading/chokidarStyles.js
+++ packages/component-loading/chokidarStyles.js
@@ -12,37 +12,39 @@ const templatePath = './src/ComponentLoadingTemplate.ts';
 // 监听文件或目录变化
 const watcher = chokidar.watch(filePath);
 
-const changeCss = ()=> {
+const changeCss = () => {
     // 编译 SCSS 文件
     const scssFilePath = path.resolve(__dirname, filePath);
-    const result = sass.renderSync({file: scssFilePath, outputStyle: 'expanded'});
+    try {
+        const result = sass.renderSync({file: scssFilePath, outputStyle: 'expanded'});
+        const cssContent = result.css.toString();
+        postcss([autoprefixer({overrideBrowserslist: ['last 30 versions', '> 0.5%', 'Firefox ESR', 'not dead']})])
+            .process(cssContent, {from: undefined})
+            .then(_result => {
+                // 添加样式后的css
+                const prefixedCss = _result.css;
 
-    const cssContent = result.css.toString();
+                // 读取模板文件
+                const templateContent = fs.readFileSync(templatePath, 'utf-8');
 
-    postcss([autoprefixer({overrideBrowserslist: ['last 30 versions', '> 0.5%', 'Firefox ESR', 'not dead']})])
-        .process(cssContent, {from: undefined})
-        .then(_result => {
-            // 添加样式后的css
-            const prefixedCss = _result.css;
+                const regex = /<style>([\s\S]*?)<\/style>/;
 
-            // 读取模板文件
-            const templateContent = fs.readFileSync(templatePath, 'utf-8');
+                const cssText = templateContent.match(regex);
 
-            const regex = /<style>([\s\S]*?)<\/style>/;
-
-            const cssText = templateContent.match(regex);
-
-            if (cssText && cssText[1] !== prefixedCss) {
-                // 将 CSS 内容插入模板中
-                const modifiedTemplateContent = templateContent.replace(regex, `<style>${prefixedCss}</style>`);
-                // 更新输出文件
-                const outputFilePath = path.resolve(__dirname, templatePath);
-                fs.writeFileSync(outputFilePath, modifiedTemplateContent);
-            }
-        })
-        .catch(error => {
-            console.error('Error processing CSS:', error);
-        });
+                if (cssText && cssText[1] !== prefixedCss) {
+                    // 将 CSS 内容插入模板中
+                    const modifiedTemplateContent = templateContent.replace(regex, `<style>${prefixedCss}</style>`);
+                    // 更新输出文件
+                    const outputFilePath = path.resolve(__dirname, templatePath);
+                    fs.writeFileSync(outputFilePath, modifiedTemplateContent);
+                }
+            })
+            .catch(error => {
+                console.error('Error processing CSS:', error);
+            });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 // 监听文件或目录变化事件
